3376cf0e7f5857268f04ed1a4657aed9f2349161
BillowsTao <taobillows@163.com>
2019-05-04 02:48:14 +0800

    Init the mobile project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Flex, WhiteSpace } from 'antd-mobile';
+
+const PlaceHolder = ({ className = '', ...restProps }) => (
+  <div className={`${className} placeholder`} {...restProps}>Block</div>
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="flex-container">
+    <div className="sub-title">Basic</div>
+    <Flex>
+      <Flex.Item><PlaceHolder /></Flex.Item>
+      <Flex.Item><PlaceHolder /></Flex.Item>
+    </Flex>
+    <WhiteSpace size="lg" />
+    <Flex>
+      <Flex.Item><PlaceHolder /></Flex.Item>
+      <Flex.Item><PlaceHolder /></Flex.Item>
+      <Flex.Item><PlaceHolder /></Flex.Item>
+    </Flex>
+    <WhiteSpace size="lg" />
+    <Flex>
+      <Flex.Item><PlaceHolder /></Flex.Item>
+      <Flex.Item><PlaceHolder /></Flex.Item>
+      <Flex.Item><PlaceHolder /></Flex.Item>
+      <Flex.Item><PlaceHolder /></Flex.Item>
+    </Flex>
+    <WhiteSpace size="lg" />
+
+    <div className="sub-title">Wrap</div>
+    <Flex wrap="wrap">
+      <PlaceHolder className="inline" />
+      <PlaceHolder className="inline" />
+      <PlaceHolder className="inline" />
+      <PlaceHolder className="inline" />
+      <PlaceHolder className="inline" />
+      <PlaceHolder className="inline" />
+      <PlaceHolder className="inline" />
+    </Flex>
+    <WhiteSpace size="lg" />
+
+    <div className="sub-title">Align</div>
+    <Flex justify="center">
+      <PlaceHolder className="inline" />
+      <PlaceHolder className="inline" />
+      <PlaceHolder className="inline" />
+    </Flex>
+    <WhiteSpace />
+    <Flex justify="end">
+      <PlaceHolder className="inline" />
+      <PlaceHolder className="inline" />
+      <PlaceHolder className="inline" />
+    </Flex>
+    <WhiteSpace />
+    <Flex justify="between">
+      <PlaceHolder className="inline" />
+      <PlaceHolder className="inline" />
+      <PlaceHolder className="inline" />
+    </Flex>
+
+    <WhiteSpace />
+    <Flex align="start">
+      <PlaceHolder className="inline" />
+      <PlaceHolder className="inline small" />
+      <PlaceHolder className="inline" />
+    </Flex>
+    <WhiteSpace />
+    <Flex align="end">
+      <PlaceHolder className="inline" />
+      <PlaceHolder className="inline small" />
+      <PlaceHolder className="inline" />
+    </Flex>
+    <WhiteSpace />
+    <Flex align="baseline">
+      <PlaceHolder className="inline" />
+      <PlaceHolder className="inline small" />
+      <PlaceHolder className="inline" />
+    </Flex>
+  </div>
   );
 }
 
