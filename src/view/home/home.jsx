6178588a275bd972892e0645ce64b8eaa98e9f49
BillowsTao <taobillows@163.com>
2019-05-16 22:10:45 +0800

    Realized the router.

---
+++ src/view/home/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './home.css';
+
+export class Home extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <h1>Home Page</h1>
+    );
+  }
+}
---
+++ src/view/my/my.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './my.css';
+
+export class My extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <h1>My Page</h1>
+    );
+  }
+}
