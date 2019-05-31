39992a5a51800cd4e20a87cb2979f2e1190f3b20
BillowsTao <taobillows@163.com>
2019-06-01 01:33:24 +0800

    构建商品类别选择弹出框

--- src/component/shop/detail/shop-detail.jsx
+++ src/component/shop/detail/shop-detail.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, withRouter } from "react-router-dom";
 import { StickyContainer, Sticky } from 'react-sticky';
 import classnames from 'classnames'; // className 操作库
 import PropTypes from "prop-types";
-import { NavBar, Icon, Tabs, ListView } from 'antd-mobile';
+import { NavBar, Icon, Tabs, ListView, Modal } from 'antd-mobile';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPhone, faEnvelope, faPlusCircle } from '@fortawesome/free-solid-svg-icons'
 import { ShopWebService } from '../../../service/shop/shop.web.service'
@@ -235,11 +235,17 @@ class Order extends React.Component {
       shopGoodsListViewData,
       // 选中店铺类别映射
       shopCategorySelectId: 0,
+      // 店铺类别商品属性
+      shopGoodsAttribute: null,
+      shopSelectedGoodsId: null,
+      showChooseStandardModal: false,
     }
 
     // 绑定 this
     this.processShopCategoryData = this.processShopCategoryData.bind(this);
     this.initShopGoodsData = this.initShopGoodsData.bind(this);
+    this.chooseGoodsStandard = this.chooseGoodsStandard.bind(this);
+    this.closeChooseGoodsStandard = this.closeChooseGoodsStandard.bind(this);
   }
 
   componentDidMount() {
@@ -277,6 +283,7 @@ class Order extends React.Component {
         {
           shopCategorySelectId: this.state.shopCategoryListData[0].shopCategoryId,
           shopCategoryListViewData: this.state.shopCategoryListViewData.cloneWithRowsAndSections(dataBlobs, categorySectionIDs, categoryRowIDs), // ReactNative 克隆视图数据
+          shopGoodsAttribute: this.state.shopCategoryListData[0].itemThinResponseList[0].attribute,  // 获取第一个类目的第一个商品的属性信息
         }
       );
     } else {
@@ -342,6 +349,27 @@ class Order extends React.Component {
     );
   }
 
+  /**
+   * 选择商品规格
+   */
+  chooseGoodsStandard(selectGoodsId) {
+
+    // 显示弹窗
+    this.setState({
+      showChooseStandardModal: true,
+      shopSelectedGoodsId: selectGoodsId,
+    });
+  }
+
+  /**
+   * 关闭选择商品规格弹出框
+   */
+  closeChooseGoodsStandard(){
+    this.setState({
+      showChooseStandardModal: false,
+    });
+  }
+
   render() {
 
     // 从数据源(data source)中接受一条数据，以及它和它所在 section 的 ID。返回一个可渲染的组件来为这行数据进行渲染。
@@ -364,8 +392,8 @@ class Order extends React.Component {
     // 从数据源(data source)中接受一条数据，以及它和它所在 section 的 ID。返回一个可渲染的组件来为这行数据进行渲染。
     const shopGoodsItemRender = (rowData, sectionID, rowID) => {
       // console.log('渲染行数据源:\nrowData:', rowData, 'sectionId:', sectionID, 'rowId:', rowID);
-      let shopCategoryGoodsData = this.state.shopCategoryListData.filter((elem)=>elem.shopCategoryId===this.state.shopCategorySelectId)[0]
-        .itemThinResponseList.filter((elem)=>elem.itemId === rowID)[0];
+      let shopCategoryGoodsData = this.state.shopCategoryListData.filter((elem) => elem.shopCategoryId === this.state.shopCategorySelectId)[0]
+        .itemThinResponseList.filter((elem) => elem.itemId === rowID)[0];
       return (
         <div
           className='order-goods__item' key={rowID}
@@ -403,7 +431,7 @@ class Order extends React.Component {
               </div>
               <div className="order-goods__content-right">
                 <div className="order-goods__control-item">
-                  <div className="order-goods__control-buy">
+                  <div className="order-goods__control-buy" onClick={()=>{this.chooseGoodsStandard(shopCategoryGoodsData.itemId);}}>
                     <FontAwesomeIcon className="order-goods__control-buy__icon" icon={faPlusCircle} size="lg" />
                   </div>
                 </div>
@@ -469,6 +497,13 @@ class Order extends React.Component {
             scrollRenderAheadDistance={500}
           />
         </div>
+        {/* 窗口组件 */}
+        <ChooseGoodsStandard
+          shopGoodsAttribute={this.state.shopGoodsAttribute}
+          shopSelectedGoodsId={this.state.shopSelectedGoodsId}
+          showChooseStandardModal={this.state.showChooseStandardModal}
+          closeModal={this.closeChooseGoodsStandard}
+        />
       </div>
     );
   }
@@ -494,6 +529,174 @@ function ShopGoodsListBodyContainer(props) {
   return (
     // 渲染自定义的区块包裹组件
     <div className="shopGoodsList-container">
+      {/** 渲染组件容器-子节点 */}
+      {props.children}
+    </div>
+  );
+}
+
+/**
+ * 购买商品选择规格对话框
+ */
+class ChooseGoodsStandard extends React.Component {
+  constructor(props) {
+    super(props);
+    // ListView 数据初始化
+    const getSectionData = (dataBlob, sectionID) => dataBlob[sectionID];
+    const getRowData = (dataBlob, sectionID, rowID) => dataBlob[rowID];
+    const shopGoodsAttributeListViewData = new ListView.DataSource({
+      getRowData,
+      getSectionHeaderData: getSectionData,
+      rowHasChanged: (row1, row2) => row1 !== row2,
+      sectionHeaderHasChanged: (s1, s2) => s1 !== s2,
+    });
+    // 初始化属性状态
+    this.state = {
+      shopGoodsAttributeListViewData, // 商品属性视图数据
+    };
+
+    this.initShopGoodsAttribute = this.initShopGoodsAttribute.bind(this);
+    this.closest = this.closest.bind(this);
+  }
+
+  componentDidMount() {
+    console.log('props:', this.props);
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    // 判断组件是否更新
+    if (this.props !== prevProps) {
+      console.log('props:', this.props);
+    }
+  }
+
+  /**
+   * 修复 Android, iOS 点击穿透
+   */
+  closest(el, selector) {
+    const matchesSelector = el.matches || el.webkitMatchesSelector || el.mozMatchesSelector || el.msMatchesSelector;
+    while (el) {
+      if (matchesSelector.call(el, selector)) {
+        return el;
+      }
+      el = el.parentElement;
+    }
+    return null;
+  }
+
+  /**
+   * 修复 Android, iOS 点击穿透
+   */
+  onWrapTouchStart = (e) => {
+    // fix touch to scroll background page on iOS
+    if (!/iPhone|iPod|iPad/i.test(navigator.userAgent)) {
+      return;
+    }
+    const pNode = this.closest(e.target, '.am-modal-content');
+    if (!pNode) {
+      e.preventDefault();
+    }
+  }
+
+  /**
+   * 处理商品属性数据
+   */
+  initShopGoodsAttribute() {
+    let categorySectionIDs = [];
+    let categoryRowIDs = [];
+    let dataBlobs = {};
+    let attribute = this.props.shopGoodsAttribute;
+    if (attribute) {
+      categorySectionIDs.push(this.props.shopSelectedGoodsId);
+      categoryRowIDs.push([]);
+      dataBlobs[this.props.shopSelectedGoodsId] = this.props.shopSelectedGoodsId;
+      Object.keys(attribute).forEach(
+        (keyName, index) => {
+          categoryRowIDs[0].push(attribute.keyName);
+          dataBlobs[attribute.keyName] = attribute.keyName;
+        }
+      );
+      this.setState(
+        {
+          shopGoodsAttribute: this.state.shopCategoryListData[0].itemThinResponseList[0].attribute,  // 获取第一个类目的第一个商品的属性信息
+          shopGoodsAttributeListViewData : this.state.shopGoodsAttributeListViewData.cloneWithRowsAndSections(dataBlobs, categorySectionIDs, categoryRowIDs),
+        }
+      );
+    } else {
+      return;
+    }
+  }
+
+  render() {
+
+    // 从数据源(data source)中接受一条数据，以及它和它所在 section 的 ID。返回一个可渲染的组件来为这行数据进行渲染。
+    const shopGoodsAttributeItemRender = (rowData, sectionID, rowID) => {
+      // console.log('渲染行数据源:\nrowData:', rowData, 'sectionId:', sectionID, 'rowId:', rowID);
+      return (
+        <div
+          key={rowID}
+          className="selectStandard-item"
+        >
+          <div className="selectStandard-item__name">
+
+          </div>
+          <div className="selectStandard-item__option">
+
+          </div>
+        </div>
+      );
+    };
+
+    return (
+      <Modal
+        visible={this.props.showChooseStandardModal}
+        transparent
+        closable
+        onClose={() => { this.props.closeModal() }}
+        title="选择商品规格"
+        footer={[{ text: 'Ok', onPress: () => { this.props.closeModal() } }]}
+        wrapProps={{ onTouchStart: this.onWrapTouchStart }}
+        afterClose={() => { console.log('After Close'); }}
+      >
+        <div className="selectStandard-layout">
+          <ListView
+            // 映射 ListView
+            ref={el => this.shopGoodsAttributeLV = el}
+            // ListView 数据源
+            dataSource={this.state.shopGoodsAttributeListViewData}
+            // 自定义 body 的包裹组件
+            renderBodyComponent={() => <ShopGoodsAttributeListBodyContainer />}
+            // 从数据源(data source)中接受一条数据，以及它和它所在 section 的 ID。返回一个可渲染的组件来为这行数据进行渲染。
+            renderRow={shopGoodsAttributeItemRender}
+            // 如果提供了此属性，一个可渲染的组件会被渲染在每一行下面，除了小节标题的前面的最后一行。在其上方的小节ID和行ID，以及邻近的行是否被高亮会作为参数传递进来。
+            renderSeparator={null}
+            // ListView 样式
+            style={{
+              height: '12em',
+              width: '100%',
+              overflowY: 'auto',
+              overflowX: 'hidden',
+            }}
+            // 每次事件循环（每帧）渲染的行数
+            pageSize={5}
+            // 在滚动的过程中，每帧最多调用一次此回调函数。调用的频率可以用 scrollEventThrottle 属性来控制。
+            onScroll={() => { }}
+            // 当一个行接近屏幕范围多少像素之内的时候，就开始渲染这一行
+            scrollRenderAheadDistance={500}
+          />
+        </div>
+      </Modal>
+    );
+  }
+}
+
+/**
+ * 店铺类别列表容器
+ */
+function ShopGoodsAttributeListBodyContainer(props) {
+  return (
+    // 渲染自定义的区块包裹组件
+    <div className="shopGoodsAttributeList-container">
       {/** 渲染组件容器-子节点 */}
       {props.children}
     </div>
